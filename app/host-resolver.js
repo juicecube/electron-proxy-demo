7701dd19b734b9ee07dfa43169871fb83ab82acd
webyom <webyom@gmail.com>
2019-11-14 10:08:11 +0800

    implement host-resolver

---
+++ app/host-resolver.js
@@ -0,0 +1,61 @@
+const {net} = require('electron');
+
+const promiseMap = {};
+
+exports.resolveHostname = function (hostname) {
+  if (!promiseMap[hostname]) {
+    promiseMap[hostname] = new Promise(function (resolve, reject) {
+      // this is a simple whitelist test, use a practical whitelist solution in production
+      if (!(/\.codemao\.cn$/).test(hostname)) {
+        resolve(hostname);
+        return;
+      }
+      let resolved = false;
+      const req = net.request({
+        url: 'http://' + hostname,
+        method: 'GET'
+      });
+      req.on('redirect', function () {
+        resolve(hostname);
+        resolved = true;
+      });
+      req.on('response', function () {
+        resolve(hostname);
+        resolved = true;
+      });
+      req.on('close', function () {
+        if (!resolved) {
+          const dnsReq = net.request('http://203.107.1.33/100000/d?host=www.aliyun.com');
+          dnsReq.on('response', function (res) {
+            const chunk = [];
+            let size = 0;
+            res.on('data', function (data) {
+              chunk.push(data);
+              size += data.length;
+            });
+            res.on('end', function () {
+              try {
+                const data = JSON.parse(Buffer.concat(chunk, size).toString());
+                if (data.ips[0]) {
+                  resolve(data.ips[0]);
+                } else {
+                  resolve(hostname);
+                }
+              } catch (err) {
+                promiseMap[hostname] = null;
+                resolve(hostname);
+              }
+            });
+            res.on('error', function () {
+              promiseMap[hostname] = null;
+              resolve(hostname);
+            });
+          });
+          dnsReq.end();
+        }
+      });
+      req.end();
+    });
+  }
+  return promiseMap[hostname];
+};
--- app/main.js
+++ app/main.js
@@ -1,21 +1,28 @@
-const { app, BrowserWindow, session } = require('electron')
+const {app, BrowserWindow, session} = require('electron');
+const {resolveHostname} = require('./host-resolver');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function initProxySession() {
-  const ses = session.fromPartition('persist:proxy')
-  ses.webRequest.onBeforeRequest((details, callback) => {
-    const url = new URL(details.url)
-    if (url.host === '127.0.0.1:5001') {
-      callback({})
-      return
+  const ses = session.fromPartition('persist:proxy');
+  ses.webRequest.onBeforeRequest(async (details, callback) => {
+    try {
+      const url = new URL(details.url);
+      const hostname = await resolveHostname(url.hostname);
+      if (hostname === url.hostname) {
+        callback({});
+      } else {
+        url.hostname = hostname;
+        callback({
+          redirectURL: url.href
+        });
+      }
+    } catch (err) {
+      callback({});
     }
-    callback({
-      redirectURL: details.url.split('127.0.0.1:5000').join('127.0.0.1:5001')
-    })
-  })
+  });
 }
 
 function createWindow () {
@@ -27,13 +34,13 @@ function createWindow () {
       partition: 'persist:proxy',
       nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
-  win.loadURL('http://127.0.0.1:5000/')
+  win.loadURL('https://staging-m.codemao.cn/');
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -41,30 +48,34 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', function () {
-  initProxySession()
-  createWindow()
-})
+  initProxySession();
+  createWindow();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
